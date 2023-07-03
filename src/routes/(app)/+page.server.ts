9347e5a384fd6d49e5447197ae823cfd94d3ad88
Antonio Sarcevic <sarcevicantonio@gmail.com>
2023-07-03 21:42:37 +0200

    helpful error handling

--- src/routes/(app)/+page.server.ts
+++ src/routes/(app)/+page.server.ts
@@ -1,10 +1,19 @@
+import { error } from '@sveltejs/kit'
+import { ClientResponseError } from 'pocketbase'
+
 export async function load({ locals, url }) {
 	const page = +(url.searchParams.get('page') || 0)
-	const post_list = await locals.pb.collection('posts').getList(page, 50, {
-		sort: '-created',
-	})
-	return {
-		post_list: structuredClone(post_list),
+	if (isNaN(page)) return error(400, 'page is NaN')
+	try {
+		const post_list = await locals.pb.collection('posts').getList(page, 50, {
+			sort: '-created',
+		})
+		return {
+			post_list: structuredClone(post_list),
+		}
+	} catch (e) {
+		if (e instanceof ClientResponseError) console.warn('ClientResponseError: is PocketBase running?')
+		throw e
 	}
 }
 
